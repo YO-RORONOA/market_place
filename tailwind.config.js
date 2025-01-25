0bfc56a8d50d1f8b86533d1bfe16949bbe122d82
ouahmane youness <youness.ouahmane4@gmail.com>
2025-01-25 17:02:00 +0100

    installed tailwind and working on header

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+     "./src/**/*.{php,html,js}", // Include files in 'src' folder
+    "./*.{php,html,js}",  // Include all JavaScript files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
