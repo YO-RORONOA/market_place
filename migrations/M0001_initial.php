77d94b0ee29e7dd6285df3953479ca400e7bdf17
youness <youness.ouahmane4@gmail.com>
2025-02-20 21:33:28 +0100

    working on database config and migrations

---
+++ migrations/M0001_initial.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+class M0001_initial
+{
+    public function up()
+    {
+        echo 'applyin migration';
+    }
+
+    public function down()
+    {
+        echo 'down migration';
+    }
+}
