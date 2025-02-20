0d9c1172eca22ba9285d23663418f5cddad2e21d
youness <youness.ouahmane4@gmail.com>
2025-02-20 22:30:34 +0100

    .env not loading

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@
  use App\controllers\AuthController;
 
 require_once __DIR__.'/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 
