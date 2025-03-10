af3398edd81f228bfeda5a13b68ff7527857ad24
ouahmane youness <youness.ouahmane4@gmail.com>
2025-03-10 20:58:52 +0000

    categorie migrations

---
+++ migrations/M0002_create_categories_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\core\Application;
+use App\migrations\Migration;
+
+
+
+class M0002_create_categories_table extends Migration
+{
+
+    public function up(): void
+    {
+        $db = Application::$app->db;
+
+        $sql = "CREATE TABLE IF NOT EXISTS categories(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        parent_id INT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        deleted_at TIMESTAMP NULL,
+        FOREIGN KEY (parent_id) REFERENCES categories(id)
+        )";
+
+        $db->pdo->exec($sql);
+
+        $sql = "INSERT INTO,categories (name) VALUES
+        ('Electronics'),
+        ('clothing'),
+        ('Home & Garden'),
+        ('Electronics'),
+        ('Books'),
+        ('Beauty & Health')";
+        
+        $db->pdo->exec($sql);
+    }
+
+
+    public function down() : void {
+        $db = Application::$app->db;
+        $sql = "DROP TABLE IF EXISTS categories;";
+        $db->pdo->exec($sql);
+        
+    }
+}
