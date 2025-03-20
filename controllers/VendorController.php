c03617e18ad0c7c93c2501c389e442b0abd45f59
ouahmane youness <youness.ouahmane4@gmail.com>
2025-03-20 18:36:30 +0000

    YOUM-2 fetching vendor products

--- controllers/VendorController.php
+++ controllers/VendorController.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\core\Application;
 use App\core\Controller;
 use App\Core\Middlewares\VendorMiddleware;
+use App\core\Request;
 use App\repositories\CategoryRepository;
 use App\repositories\ProductRepository;
 
@@ -26,6 +28,28 @@ class VendorController extends Controller
         return $this->render('vendor/dashboard', [
             'title' => 'vendor Dashboard'
         ]);
+    }
+
+    public function products(Request $request)
+    {
+        $vendorId = Application::$app->session->get('user')['id'] ?? 0;
+        $page = (int)$request->getQuery('page', 1);
+        $limit = 10;
+        $offset = ($page - 1) * $limit;
+
+        $products = $this->productRepository->findByVendor($vendorId, $limit, $offset);
+        $totalProducts = $this->productRepository->countProducts(['vendor_id' => $vendorId]);
+        $totalPages = ceil($totalProducts / $limit);
+
+        return $this->render('vendor/products/index', [
+            'products' => $products,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
+            'totalProducts' => $totalProducts,
+            'title' => 'My Products'
+        ]);
+
+
     }
 
     
