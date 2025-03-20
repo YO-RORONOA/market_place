6e8408348747d304953a54fb02110727f7fa6cdf
ouahmane youness <youness.ouahmane4@gmail.com>
2025-03-20 18:25:46 +0000

    YOUM-2 vendor controller dash view

--- controllers/VendorController.php
+++ controllers/VendorController.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\core\Controller;
+use App\Core\Middlewares\VendorMiddleware;
 use App\repositories\CategoryRepository;
 use App\repositories\ProductRepository;
 
@@ -8,4 +9,25 @@ class VendorController extends Controller
 {
     private ProductRepository $productRepository;
     private CategoryRepository $categoryRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->productRepository = new ProductRepository();
+        $this->categoryRepository = new CategoryRepository();
+
+        $this->registerMiddleware(new VendorMiddleware);
+        $this->setLayout('vendor');
+    }
+
+
+    public function dashboard()
+    {
+        return $this->render('vendor/dashboard', [
+            'title' => 'vendor Dashboard'
+        ]);
+    }
+
+    
+
 }
