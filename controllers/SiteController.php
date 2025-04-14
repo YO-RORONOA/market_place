25a72c90f5136eb00d9f510cfedb9975e0dac982
ouahmane youness <youness.ouahmane4@gmail.com>
2025-04-14 14:00:11 +0100

    updated home method for home page display

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,14 +6,61 @@ namespace App\controllers;
 use App\core\Application;
 use App\core\Controller;
 use App\core\Request;
-
-
+use App\repositories\CategoryRepository;
+use App\repositories\ProductRepository;
 
 class SiteController extends Controller
 {
+    private CategoryRepository $categoryRepository;
+    private ProductRepository $productRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->categoryRepository = new CategoryRepository();
+        $this->productRepository = new ProductRepository();
+    }
     public function home()
     {
-        return $this->render('home');
+        $categories = $this->categoryRepository->getMainCategories();
+        
+        $newArrivals = $this->productRepository->findAll(
+            ['status' => 'active'], 
+            false,
+            'created_at DESC',
+            8 
+        );
+        
+        $popularProducts = $this->productRepository->findAll(
+            ['status' => 'active'], 
+            false,
+            'created_at DESC',
+            8
+        );
+        
+        $categoryProducts = [];
+        foreach ($categories as $category) {
+            $products = $this->productRepository->findByCategory(
+                $category['id'],
+                4, 
+                0
+            );
+            
+            if (!empty($products)) {
+                $categoryProducts[$category['id']] = [
+                    'name' => $category['name'],
+                    'products' => $products
+                ];
+            }
+        }
+        
+        return $this->render('home/index', [
+            'categories' => $categories,
+            'newArrivals' => $newArrivals,
+            'popularProducts' => $popularProducts,
+            'categoryProducts' => $categoryProducts,
+            'title' => 'Welcome to YOU/Market'
+        ]);
     }
 
     public function login()
